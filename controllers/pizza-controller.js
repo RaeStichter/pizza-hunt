9e06842bc249eff1a56c8820df770d7c35f039ce
Rae Stichter <raestichter@gmail.com>
2021-03-04 23:43:32 -0500

    user modle and testing

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,76 @@
+const { Pizza } = require('../models');
+const { db } = require('../models/Pizza');
+
+const pizzaController = {
+  // the functions will go in here as methods
+
+  // get all pizzas - this will serve as the callback function for GET /api/pizzas
+  // this uses the mongoose .find() method which is similar to sequelize .findAll()
+  getAllPizza(req, res) {
+      Pizza.find({})
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+      });
+  },
+
+  // get one pizza by id
+  // thos uses mongoose .findOne() to find a single pizza by id.  
+  // instead of accessing the entire req, we've destructured params out of it
+  getPizzaById({ params }, res) {
+      Pizza.findOne({_id: params.id })
+      .then(dbPizzaData => {
+          // If no pizza is found, send 404
+          if (!dbPizzaData) {
+              res.status(404).json({ message: 'No pizza found with this id!'});
+              return;
+          }
+          res.json(dbPizzaData);
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+      });
+  },
+
+  // create Pizza
+  //Destructure the info out of the body section from the express.js req object
+  createPizza({ body }, res) {
+      Pizza.create(body)
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.status(400).json(err));
+  },
+
+  // update pizza by id
+  // this is the mongoose version of find one with the id and update
+  // need the third arguement with { new: true }, otherwise, it will send back the original statement
+  updatePizza({ params, body }, res) {
+      Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then(dbPizzaData => {
+          if (!dbPizzaData) {
+              res.status(404).json({ message: 'No pizza found with this id!' });
+              return;
+          }
+          res.json(dbPizzaData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
+
+  // delete pizza
+  // this mongoose function will find the document to be returned and also delete it from the database
+  deletePizza({ params }, res) {
+      Pizza.findOneAndDelete({ _id: params.id })
+      .then(dbPizzaData => {
+          if (!dbPizzaData) {
+              res.status(404).json({ message: 'No pizza found with this id!' });
+              return;
+          }
+          res.json(dbPizzaData);
+      })
+      .catch(err => res.status(400).json(err));
+  }
+
+};
+
+module.exports = pizzaController;
---
+++ models/Pizza.js
@@ -0,0 +1,27 @@
+// Input the Schema constructor and the model function from the mongoose library (not the whole library)
+const { Schema, model } = require('mongoose');
+
+// Data that will be stored for the pizza model
+const PizzaSchema = new Schema({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    toppings: []
+});
+
+// create the Pizza model using the PizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+// export the Pizza model
+module.exports = Pizza;
